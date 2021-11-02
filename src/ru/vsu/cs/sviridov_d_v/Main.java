73ea846e01ee44b06cd1ef45260caee643d9a068
dansvr25 <sviridov.danila@icloud.com>
2021-11-03 00:55:11 +0300

    Task1

--- src/ru/vsu/cs/sviridov_d_v/Main.java
+++ src/ru/vsu/cs/sviridov_d_v/Main.java
@@ -10,13 +10,10 @@ public class Main {
         Locale.setDefault(Locale.ROOT);
 
         double amount = readNumber("Введите сумму вклада (₽): ");
-        checkNumber(amount);
 
         double term = readNumber("Введите срок вклада (годы): ");
-        checkNumber(term);
 
         double rate = readNumber("Введите процентную ставку вклада (%): ");
-        checkNumber(rate);
 
         double sum = countSum(amount, rate, term);
 
@@ -24,17 +21,20 @@ public class Main {
 
     }
 
-
     static double readNumber(String text) {
         System.out.print(text);
         Scanner scn = new Scanner(System.in);
-        return scn.nextDouble();
-    }
+        double value = scn.nextDouble();
 
-    static void checkNumber(double value) {
         if (value <= 0) {
-            System.out.print("Значение не может быть отрицательным или раным нулю");
+            do {
+                System.out.println("Значение не может быть отрицательным или раным нулю");
+                System.out.print(text);
+                scn = new Scanner(System.in);
+                value = scn.nextDouble();
+            }   while (value <= 0);
         }
+        return value;
     }
 
     static double countSum(double pv, double r, double n) {
